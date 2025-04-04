f4461608523100d83ac458ed538e30ce5f0a06b6
TalantRahimberdiev <76486068+TalantRahimberdiev@users.noreply.github.com>
2025-04-04 15:50:49 +0600

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -38,3 +38,53 @@ class Solution:
 
         res = [val * 10**(len(lst) - 1 - i) for i, val in enumerate(lst)]
         return sum(res)
+
+def roman_to_int(roman):
+    dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    converted = [dict[symbol] for symbol in roman]
+
+    to_be_kept=converted.copy()
+    i = 0
+    while i < len(to_be_kept) - 1:
+        if to_be_kept[i] < to_be_kept[i + 1]:
+            to_be_kept[i] = 0
+            to_be_kept[i + 1] = 0
+            i += 2
+        else:
+            i += 1
+
+    to_be_subtracted=list()
+    for j in range(len(converted)-1):
+        if converted[j] < converted[j+1]:
+            to_be_subtracted.append(converted[j+1]-converted[j])
+
+    return sum(to_be_kept)+sum(to_be_subtracted)
+
+
+print(roman_to_int('MCMXCVI'))
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        total = 0
+        prev = 0
+
+        for char in reversed(s):
+            value = roman[char]
+            if value < prev:
+                total -= value
+            else:
+                total += value
+                prev = value
+
+        return total
